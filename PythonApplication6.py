525fb15fd6f794ec2ca0b93c7e6db2484d71b986
Kdog4775 <66104108+Kdog4775@users.noreply.github.com>
2021-08-31 12:23:23 -0700

    Update PythonApplication6.py

--- PythonApplication6.py
+++ PythonApplication6.py
@@ -1,4 +1,6 @@
 import  sqlite3
+
+
 conn  =  sqlite3 . connect ( 'mydatabase.db' )
 cursor  =  conn.cursor ()
 #create the table 
@@ -8,13 +10,16 @@ s_id = input('Snumber:')
 s_name = input('Name:')
 s_print = input('Print:')
 s_date = input('Date:')
+#Adding to the table after a user does an input
 cursor.execute("""
 INSERT INTO CGI(Snumber, Name, Print, Date)
 VALUES (?,?,?,?)
 """, (s_id, s_name, s_print, s_date))
 conn.commit ()
 print ( 'Data entered successfully.' )
+#telling you if it done correctly
 conn . close ()
 if (conn):
   conn.close()
   print("\nThe SQLite connection is closed.")
+#saying it done
